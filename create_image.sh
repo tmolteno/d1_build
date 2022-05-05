e6cb8474641ecc311debf1fec75c099ff9ae30a9
tim <tim@elec.ac.nz>
2022-05-05 13:23:28 +1200

    Parse the loopback device properly

--- create_image.sh
+++ create_image.sh
@@ -12,35 +12,37 @@ IMG=${OUTPORT}/licheerv.img
 dd if=/dev/zero of=${IMG} bs=1M count=3500
 
 # Setup Loopback device
-LOOP=`losetup -f --show ${IMG}`
+LOOP=`losetup -f --show ${IMG} | cut -d'/' -f3`
+LOOPDEV=/dev/${LOOP}
 
 # Create partitions
-sfdisk ${LOOP} < ${OUTPORT}/disk_layout.sfdisk
-kpartx -av ${LOOP}
-mkfs.vfat /dev/mapper/loop0p1
-mkfs.ext4 /dev/mapper/loop0p2
+sfdisk ${LOOPDEV} < ${OUTPORT}/disk_layout.sfdisk
+kpartx -av ${LOOPDEV}
+mkfs.vfat /dev/mapper/${LOOP}p1
+mkfs.ext4 /dev/mapper/${LOOP}p2
 
 # Burn U-boot
-dd if=/dev/zero of=${LOOP} bs=1k count=1023 seek=1
+dd if=/dev/zero of=${LOOPDEV} bs=1k count=1023 seek=1
 dd if=${OUTPORT}/boot0_sdcard_sun20iw1p1.bin of=${LOOP} bs=8192 seek=16
 
 # Copy files https://linux-sunxi.org/Allwinner_Nezha
-dd if=${OUTPORT}/u-boot.toc1 of=${LOOP} bs=512 seek=32800
-dd if=${OUTPORT}/u-boot.toc1 of=${LOOP} bs=512 seek=24576
+dd if=${OUTPORT}/u-boot.toc1 of=${LOOPDEV} bs=512 seek=32800
+dd if=${OUTPORT}/u-boot.toc1 of=${LOOPDEV} bs=512 seek=24576
 
 # Copy Files, first the boot partition
-# mount /dev/mapper/loop0p1 /mnt
-# cp ${uImage_dir}/uImage /mnt
-# cp ${script.bin_dir)/script.bin /mnt
-# cp ${uEnv.txt_dir}/uEnv.txt /mnt
-# umount /mnt
+MNTPOINT=/rvmnt
 
+# mount /dev/mapper/loop0p1 ${MNTPOINT}
+# cp ${uImage_dir}/uImage ${MNTPOINT}
+# cp ${script.bin_dir)/script.bin ${MNTPOINT}
+# cp ${uEnv.txt_dir}/uEnv.txt ${MNTPOINT}
+# umount ${MNTPOINT}
 
-mount /dev/mapper/loop0p2 /mnt
-cp -a ${OUTPORT}/rv64-port/* /mnt
-umount /mnt
+mkdir -p ${MNTPOINT}
+mount /dev/mapper/${LOOP}p2 ${MNTPOINT}
+cp -a ${OUTPORT}/rv64-port/* ${MNTPOINT}
+umount ${MNTPOINT}
 
 # Clean Up
-kpartx -d ${LOOP}
-losetup -d ${LOOP}
-umount ${LOOP}
+kpartx -d ${LOOPDEV}
+losetup -d ${LOOPDEV}

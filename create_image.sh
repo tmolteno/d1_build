a6a00013a57dea481e36e9e717958a6ef06b2359
tim <tim@elec.ac.nz>
2022-05-05 13:47:07 +1200

    Better commants

--- create_image.sh
+++ create_image.sh
@@ -9,11 +9,15 @@
 OUTPORT=$1
 
 IMG=${OUTPORT}/licheerv.img
+
+echo "Creating Blank Image ${IMG}"
+
 dd if=/dev/zero of=${IMG} bs=1M count=3500
 
 # Setup Loopback device
 LOOP=`losetup -f --show ${IMG} | cut -d'/' -f3`
 LOOPDEV=/dev/${LOOP}
+echo "Using loopback device ${LOOPDEV}"
 
 # Create partitions
 sfdisk ${LOOPDEV} < ${OUTPORT}/disk_layout.sfdisk
@@ -22,6 +26,8 @@ mkfs.vfat /dev/mapper/${LOOP}p1
 mkfs.ext4 /dev/mapper/${LOOP}p2
 
 # Burn U-boot
+echo "Burning u-boot to ${LOOPDEV}"
+
 dd if=/dev/zero of=${LOOPDEV} bs=1k count=1023 seek=1
 dd if=${OUTPORT}/boot0_sdcard_sun20iw1p1.bin of=${LOOP} bs=8192 seek=16
 
@@ -30,6 +36,7 @@ dd if=${OUTPORT}/u-boot.toc1 of=${LOOPDEV} bs=512 seek=32800
 dd if=${OUTPORT}/u-boot.toc1 of=${LOOPDEV} bs=512 seek=24576
 
 # Copy Files, first the boot partition
+echo "Copying files to boot partition ${LOOPDEV}"
 MNTPOINT=/rvmnt
 
 # mount /dev/mapper/loop0p1 ${MNTPOINT}
@@ -37,6 +44,8 @@ MNTPOINT=/rvmnt
 # cp ${script.bin_dir)/script.bin ${MNTPOINT}
 # cp ${uEnv.txt_dir}/uEnv.txt ${MNTPOINT}
 # umount ${MNTPOINT}
+
+echo "Copying files to main partition ${LOOPDEV}"
 
 mkdir -p ${MNTPOINT}
 mount /dev/mapper/${LOOP}p2 ${MNTPOINT}

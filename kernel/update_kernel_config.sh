388bc796f7334a02df4aae70d36fa97fa94c3ab5
tim <tim@elec.ac.nz>
2022-05-13 16:18:31 +1200

    Add link to potential better kernel config

--- kernel/update_kernel_config.sh
+++ kernel/update_kernel_config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Author Tim Molteno
 # Update the mainline kernel nezha_defconfig
+# TODO check this link... https://github.com/orangecms/linux/commit/1c493a6ef452189ae6820bb8282e8554e7527473#diff-21cd816c673272d3392dba0a524ec1a068e9229ee784cfb106c02aa89e26f7d1R179
 
 CONF_FILE=/build/linux/arch/riscv/configs/nezha_defconfig
 function add_config() {

872897e8202dee4d062a16ca97e693263bbf1441
Ilya Esin <iyesin@users.noreply.github.com>
2022-12-14 22:30:35 +0100

    Sprinkle with quotes to make linter happy

--- scripts/create_image.sh
+++ scripts/create_image.sh
@@ -6,47 +6,47 @@
 
 # Make Image the first parameter of this script is the directory containing all the files needed
 # This is done to allow the script to be run outside of Docker for testing.
-OUTPORT=$1
+OUTPORT="$1"
 
 
-KERNEL_TAG="$(echo ${KERNEL_TAG} | tr '/' '_')"
+KERNEL_TAG="$(echo "${KERNEL_TAG}" | tr '/' '_')"
 IMG_NAME="${BOARD}_gcc_${GNU_TOOLS_TAG}_kernel_${KERNEL_TAG}.img"
-IMG=${OUTPORT}/${IMG_NAME}
+IMG="${OUTPORT}/${IMG_NAME}"
 
 echo "Creating Blank Image ${IMG}"
 
-dd if=/dev/zero of=${IMG} bs=1M count=${DISK_MB}
+dd if=/dev/zero "of=${IMG}" bs=1M "count=${DISK_MB}"
 
 # Setup Loopback device
-LOOP=`losetup -f --show ${IMG} | cut -d'/' -f3`
-LOOPDEV=/dev/${LOOP}
+LOOP="$(losetup -f --show "${IMG}" | cut -d'/' -f3)"
+LOOPDEV="/dev/${LOOP}"
 echo "Partitioning loopback device ${LOOPDEV}"
 
 
 # dd if=/dev/zero of=${LOOPDEV} bs=1M count=200
-parted -s -a optimal -- ${LOOPDEV} mklabel gpt
-parted -s -a optimal -- ${LOOPDEV} mkpart primary ext2 40MiB 100MiB
-parted -s -a optimal -- ${LOOPDEV} mkpart primary ext4 100MiB -1GiB
-parted -s -a optimal -- ${LOOPDEV} mkpart primary linux-swap -1GiB 100%
+parted -s -a optimal -- "${LOOPDEV}" mklabel gpt
+parted -s -a optimal -- "${LOOPDEV}" mkpart primary ext2 40MiB 100MiB
+parted -s -a optimal -- "${LOOPDEV}" mkpart primary ext4 100MiB -1GiB
+parted -s -a optimal -- "${LOOPDEV}" mkpart primary linux-swap -1GiB 100%
 
-kpartx -av ${LOOPDEV}
+kpartx -av "${LOOPDEV}"
 
-mkfs.ext2 /dev/mapper/${LOOP}p1
-mkfs.ext4 /dev/mapper/${LOOP}p2
-mkswap /dev/mapper/${LOOP}p3
+mkfs.ext2 "/dev/mapper/${LOOP}p1"
+mkfs.ext4 "/dev/mapper/${LOOP}p2"
+mkswap "/dev/mapper/${LOOP}p3"
 
 # Burn U-boot
 echo "Burning u-boot to ${LOOPDEV}"
 
 # Copy files https://linux-sunxi.org/Allwinner_Nezha
-dd if=/builder/u-boot-sunxi-with-spl.bin of=${LOOPDEV} bs=1024 seek=128
+dd if=/builder/u-boot-sunxi-with-spl.bin "of=${LOOPDEV}" bs=1024 seek=128
 
 # Copy Files, first the boot partition
 echo "Mounting  partitions ${LOOPDEV}"
 BOOTPOINT=/sdcard_boot
 
-mkdir -p ${BOOTPOINT}
-mount /dev/mapper/${LOOP}p1 ${BOOTPOINT}
+mkdir -p "${BOOTPOINT}"
+mount "/dev/mapper/${LOOP}p1" "${BOOTPOINT}"
 
 # Boot partition
 cp /builder/Image.gz "${BOOTPOINT}/"
@@ -55,21 +55,21 @@ cp /builder/Image.gz "${BOOTPOINT}/"
 cp /builder/boot.scr "${BOOTPOINT}/"
 cp /builder/ov_lichee_rv_mini_lcd.dtb "${BOOTPOINT}/"
 
-umount ${BOOTPOINT}
-rm -rf ${BOOTPOINT}
+umount "${BOOTPOINT}"
+rm -rf "${BOOTPOINT}"
 
 
 # Now create the root partition
 MNTPOINT=/sdcard_rootfs
-mkdir -p ${MNTPOINT}
-mount /dev/mapper/${LOOP}p2 ${MNTPOINT}
+mkdir -p "${MNTPOINT}"
+mount "/dev/mapper/${LOOP}p2" "${MNTPOINT}"
 
 # Copy the rootfs
-cp -a /builder/rv64-port/* ${MNTPOINT}
+cp -a /builder/rv64-port/* "${MNTPOINT}"
 
 # Set up the rootfs
-chroot ${MNTPOINT} /bin/sh /setup_rootfs.sh
-rm ${MNTPOINT}/setup_rootfs.sh
+chroot "${MNTPOINT}" /bin/sh /setup_rootfs.sh
+rm "${MNTPOINT}/setup_rootfs.sh"
 
 # Set up fstab
 cat >> "${MNTPOINT}/etc/fstab" <<EOF
@@ -81,13 +81,13 @@ EOF
 
 # Clean Up
 echo "Cleaning Up..."
-umount ${MNTPOINT}
-rm -rf ${MNTPOINT}
+umount "${MNTPOINT}"
+rm -rf "${MNTPOINT}"
 
-kpartx -d ${LOOPDEV}
-losetup -d ${LOOPDEV}
+kpartx -d "${LOOPDEV}"
+losetup -d "${LOOPDEV}"
 
 # Now compress the image
 echo "Compressing the image: ${IMG}"
 
-(cd ${OUTPORT}; xz -T0 -9 ${IMG})
+(cd "${OUTPORT}" && xz -T0 -9 "${IMG}")

c588353f863c251eae606fd50224bb91c963ceda
Ilya Esin <iyesin@users.noreply.github.com>
2022-12-25 21:39:41 +0100

    I extended `/boot` partition a bit to allow it to carry more than one kernel version

--- scripts/create_image.sh
+++ scripts/create_image.sh
@@ -25,8 +25,8 @@ echo "Partitioning loopback device ${LOOPDEV}"
 
 # dd if=/dev/zero of=${LOOPDEV} bs=1M count=200
 parted -s -a optimal -- "${LOOPDEV}" mklabel gpt
-parted -s -a optimal -- "${LOOPDEV}" mkpart boot ext2 40MiB 100MiB
-parted -s -a optimal -- "${LOOPDEV}" mkpart root ext4 100MiB -1GiB
+parted -s -a optimal -- "${LOOPDEV}" mkpart boot ext2 40MiB 300MiB
+parted -s -a optimal -- "${LOOPDEV}" mkpart root ext4 300MiB -1GiB
 parted -s -a optimal -- "${LOOPDEV}" mkpart swap linux-swap -1GiB 100%
 
 kpartx -av "${LOOPDEV}"
